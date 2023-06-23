3637ec2879d8213ff591a33eed2327e4e2f39823
adam <adam.erben@seznam.cz>
2023-06-23 15:50:51 +0200

    Init

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
 const navMenu = document.getElementById("nav__menu");
 const sidePanel = document.getElementById("side_panel");
+const images = document.querySelectorAll(".fotka");
+const modal = document.querySelector(".modal");
+const modalImage = document.querySelector(".modal-image");
 
 navMenu.onclick = () => {
   navMenu.classList.toggle("menuActive");
   sidePanel.classList.toggle("menuActive");
   document.body.classList.toggle("overflow");
 };
+
+modal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+images.forEach((image) => {
+  image.addEventListener("click", (event) => {
+    modalImage.src = event.target.src;
+    modal.style.display = "flex";
+  });
+});
